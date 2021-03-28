b0f92223599c54659b058d3914a205e898869bae
Juan Manuel Calderon <sayroback@gmail.com>
2021-03-28 07:06:49 -0600

    Iniciando express

--- intro-node/index.js
+++ intro-node/index.js
@@ -1,10 +1,11 @@
 const { response } = require("express");
 
-const express = require("express");
-const app = express();
+const express = require("express"); // llamar la dependencia que instalamos
+const app = express(); // ejecutamos express
 
 app.get("*", (request, response) => {
-  response.send({ messange: "Chanchito Feliz" });
+  // get *  -> va a recibir todas la peticiones
+  response.send({ messange: "Chanchito Feliz" }); // response send -> para mandar
 });
 
-app.listen(3000, () => console.log("Nuestro servidor esta escuchando"));
+app.listen(3000, () => console.log("Nuestro servidor esta escuchando")); // iniciar en el puerto 3000
