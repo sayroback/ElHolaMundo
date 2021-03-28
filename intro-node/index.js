1ce3983fe3982474104221209732486b68c88470
sayroback <46367290+sayroback@users.noreply.github.com>
2021-03-28 07:54:02 -0600

    Update index.js

--- intro-node/index.js
+++ intro-node/index.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const { response } = require("express");
-
-const express = require("express");
-const app = express();
-
-app.get("*", (request, response) => {
-  response.send({ messange: "Chanchito Feliz" });
-});
-
-app.listen(3000, () => console.log("Nuestro servidor esta escuchando"));
-=======
-const { response } = require("express");
-
 const express = require("express"); // llamar la dependencia que instalamos
 const app = express(); // ejecutamos express
 
@@ -21,4 +7,3 @@ app.get("*", (request, response) => {
 });
 
 app.listen(3000, () => console.log("Nuestro servidor esta escuchando")); // iniciar en el puerto 3000
->>>>>>> 86ed2a3f2fbe79f73efd0dbd5c54ed538989edc2
